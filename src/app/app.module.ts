2550c0d78205ebf171583f6ec52e65ce7d2801d9
Fazri Anggriawan <fazri.anggriawan@gmail.com>
2022-01-11 09:09:05 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { TabViewModule } from "primeng/tabview";
+import { ButtonModule } from "primeng/button";
+import { CalendarModule } from "primeng/calendar";
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule,
+    ButtonModule,
+    CalendarModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
