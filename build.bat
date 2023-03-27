edbb155e703ce88257d0b4d5643a76ae17a64523
Simrs Mandiri Client Salak <fazri.anggriawan@gmail.com>
2023-03-27 11:06:54 +0700

    ambon

--- build.bat
+++ build.bat
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 ng build --deploy-url=./ --base-href=./ --output-path=publish --configuration production
-=======
-ng build --deploy-url=./ --base-href=./ --configuration production
->>>>>>> ambon
+
--- src/app/config.ts
+++ src/app/config.ts
@@ -2,9 +2,10 @@ let host = 'http://rstambon.co.id/';
 // let host = 'http://localhost/';
 let hostSimrs = 'http://36.94.176.218/';
 
+
 export const config = {
-    api: function (url: string) { return host + 'bihealth_api/index.php/' + url },
-    api_vclaim: function (url: string) { return host + 'bihealth_vclaim/public/' + url },
-    api_simrs: function (url: string) { return hostSimrs + 'bihealth_api/vclaim/index.php/' + url },
+    api: function (url: string) { return host + 'api/index.php/' + url },
+    api_vclaim: function (url: string) { return hostSimrs + 'vclaim/public/' + url },
+    api_simrs: function (url: string) { return hostSimrs + 'api/vclaim/index.php/' + url },
     host: host
 }
